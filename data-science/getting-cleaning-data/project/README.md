bb139504f1fffad0b9b6a49893a027533d421ffe
durgesh.verma <reply_2verma@yahoo.com>
2014-12-21 15:49:04 -0500

    project submission for getdata-016 course
    project submission for getdata-016 course

---
+++ data-science/getting-cleaning-data/project/README.md
@@ -0,0 +1,66 @@
+### Introduction
+
+This project assignment demonstrates the learning ability to collect, analyze, and clean a data set. 
+The goal is to prepare tidy data that can be used for later analysis. 
+The submission has output of data analysis described as below:
+1. run_analysis.R : script that implements requirements and generate output file
+2. tidy_UCI_HAR_Dataset.csv : output file
+	- tabular data (180 rows * 68 cols) in csv form
+		- 2 columns subject_id and act_label_name represents columns on which output data is categorized
+		- 66 columns (called features) out of 561 are taken from input data source representing mean and standard deviation only
+3. codebook.md : code book describes the meta data definitions of output file
+4. README.md : this file
+
+### Requirement for project assignment
+1. create one R script called run_analysis.R that does the following
+	- Merges the training and the test sets to create one data set.
+	- Extracts only the measurements on the mean and standard deviation for each measurement. 
+	- Uses descriptive activity names to name the activities in the data set
+	- Appropriately labels the data set with descriptive variable names. 
+	- From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+### Raw data
+1. download the data source file (https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip)
+2. unzip file
+3. read and follow README.txt to understand the data definition
+4. useful links
+	- http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
+	- http://www.insideactivitytracking.com/data-science-activity-tracking-and-the-battle-for-the-worlds-top-sports-brand/
+
+### Files required for data analysis 
+(all files are part of "Input data source")
+run_analysis.R script depends upon below files which are provided and are part of "Raw data". Definition of each file is taken from README.txt of "Raw data".
+1. 'features.txt': List of all features.
+2. 'activity_labels.txt': Links the class labels with their activity name.
+3. 'train/X_train.txt': Training set.
+4. 'train/y_train.txt': Training labels.
+5. 'train/subject_train.txt': Each row identifies the subject who performed the activity for each window sample. Its range is from 1 to 30.
+6. 'test/X_test.txt': Test set.
+7. 'test/y_test.txt': Test labels.
+8. 'test/subject_test.txt': Each row identifies the subject who performed the activity for each window sample. Its range is from 1 to 30.
+			
+### About the R script run_analysis.R
+1. function run_analysis
+This function is the main starting point of data analysis. This function 
+	- downloads the raw data file, unzip it, and saves in local system
+	- set the working directory to the folder unzipped 'UCI HAR Dataset'
+	- read and store activities definition data in local variable
+	- read and store features definition data in local variable
+	- creates vector of selected features (mean and standard deviation columns)
+	- prepare test and train data by calling function collectDataBySelectedFeatures
+	- merge test and train data and join whole dataset with activities by activity id
+	- clean up column names of dataset (remove chars ['(', ')'], replace '-' with '_')
+	- aggregate dataset by subjects and activities
+	- saves output data to "./../tidy_UCI_HAR_Dataset.csv" file
+2. function collectDataBySelectedFeatures
+This function 
+	- reads the X data file, y data file, subject data file
+	- binds 3 files into one dataset
+	- select columns based on given vector of selected features 
+3. function collectDataByAllFeatures
+This function 
+	- reads the X data file, y data file, subject data file
+	- binds 3 files into one dataset
+
+### Contributor
+	- Student Coursera class getdata-016
---
+++ data-science/getting-cleaning-data/project/run_analysis.R
@@ -0,0 +1,102 @@
+## runs analysis on the given data
+## source of data: 
+##      https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+## see README.txt file to know more about meta data 
+run_analysis <- function() {
+    library(data.table)
+    setwd("/Users/durgeshverma/Documents/personal/durgesh/coursera/data-science/course-3-getting-and-cleaning-data")
+    
+    filename <- 'UCI HAR Dataset.zip'
+    if (!file.exists(filename)) {
+        download.file('https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip', filename, method='curl')
+        unzip(filename, exdir="./")
+    }
+    setwd(paste(getwd(),'UCI HAR Dataset',sep='/'))
+    
+    ## read list of activities selected to perform the experiment
+    activities = read.table("./activity_labels.txt")
+    colnames(activities) <- c("act_label_id", "act_label_name")
+    
+    ## read list of features applied on the experimental data
+    all_features = read.table("./features.txt")
+    features_of_interest <- c(grep("-mean\\(\\)",all_features$V2), grep("-std\\(\\)",all_features$V2))
+    
+    ## 1. Merges the train and the test sets to create one data set.
+    ## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+    
+    ## read test dataset
+    test_dataset <- collectDataBySelectedFeatures(all_features, features_of_interest, "./test/X_test.txt", "./test/y_test.txt", "./test/subject_test.txt")
+    ## add column to identify data belongs to test dataset
+    test_dataset <- cbind(data.frame(dataset_name=rep("test", nrow(test_dataset))), test_dataset)
+    
+    ## read train dataset
+    train_dataset <- collectDataBySelectedFeatures(all_features, features_of_interest, "./train/X_train.txt", "./train/y_train.txt", "./train/subject_train.txt")
+    ## add column to identify data belongs to train dataset
+    train_dataset <- cbind(data.frame(dataset_name=rep("train", nrow(train_dataset))), train_dataset)
+    
+    ## merge test and train dataset
+    merged_data <- rbind(test_dataset, train_dataset)
+    
+    ## 3. Uses descriptive activity names to name the activities in the data set
+    ## 4. Appropriately labels the data set with descriptive variable names.
+    
+    ## add activity label name to merged dataset
+    merged_data <- plyr::join(merged_data, activities, by = "act_label_id", type = "left")
+    
+    ## clean up column names in merged dataset
+    colnames(merged_data) <- gsub("\\(\\)","",colnames(merged_data))
+    colnames(merged_data) <- gsub("\\-","_",colnames(merged_data))
+
+    ## 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+    
+    ## group merged dataset by subject and activity
+    by_subjects_activities <- aggregate(
+        select(merged_data,-(dataset_name:subject_id), -act_label_name),                    ## select quantitative data
+        list(subject_id=merged_data$subject_id,act_label_name=merged_data$act_label_name),  ## define group by
+        mean                                                                                ## function to apply on selected quantitative data
+    )
+    
+    write.csv(by_subjects_activities, file="./../tidy_UCI_HAR_Dataset.csv")
+    
+    by_subjects_activities
+}
+
+## returns dataset as outcome of reading given dataset file, filtering the selected features, and then combine it with subject and activities data
+## input parameters:
+##      all_features - all features used in this dataset
+##      selected_features - final selection of features required in output dataset
+##      main_filename - main file (X_*.txt) to read this dataset
+##      activity_filename - activity file of this dataset
+##      subject_filename - subject file of this dataset
+## returns:
+##      data frame
+collectDataBySelectedFeatures <- function(all_features, selected_features, main_filename, activity_filename, subject_filename) {
+    main_data = read.table(main_filename)
+    colnames(main_data) <- all_features$V2
+    main_data_selected_features = main_data[, selected_features]
+    main_data_act_labels = read.table(activity_filename)
+    colnames(main_data_act_labels) <- c("act_label_id")
+    main_data_subjects = read.table(subject_filename)
+    colnames(main_data_subjects) <- c("subject_id")
+    main_data <- cbind(main_data_act_labels, main_data_subjects, main_data_selected_features)
+    main_data
+}
+
+## returns dataset as outcome of reading given dataset file, and then combine it with subject and activities data
+## input parameters:
+##      all_features - all features used in this dataset
+##      main_filename - main file (X_*.txt) to read this dataset
+##      activity_filename - activity file of this dataset
+##      subject_filename - subject file of this dataset
+## returns:
+##      data frame
+collectDataByAllFeatures <- function(all_features, main_filename, activity_filename, subject_filename) {
+    main_data = read.table(main_filename)
+    colnames(main_data) <- all_features$V2
+    main_data_act_labels = read.table(activity_filename)
+    colnames(main_data_act_labels) <- c("act_label_id")
+    main_data_subjects = read.table(subject_filename)
+    colnames(main_data_subjects) <- c("subject_id")
+    main_data <- cbind(main_data_act_labels, main_data_subjects, main_data)
+    main_data
+}
